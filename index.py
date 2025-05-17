43b4cede2b2b670a0ec3028d30dbe624fe2142cf
Samuel Barbosa Santos <125471851+samuelbsantos66@users.noreply.github.com>
2025-05-17 10:32:35 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,18 @@
+class Maquina_Norma:
+    def _init_(self):
+        self.registradores = []
+    
+    def add(reg):
+        return reg+1
+
+    def sub(reg):
+        return reg-1
+
+    def zero(reg):
+        if reg==0:
+            return true
+        else
+            return false
+
+
+
